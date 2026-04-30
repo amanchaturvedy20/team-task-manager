14d5b57ac215924759e54eafffc9516277568c2d
Aman Chaturvedy <amanchaturvedy20@gmail.com>
2026-05-01 04:07:14 +0530

    feat: serve frontend from backend for 1-click deployment

--- server/src/index.js
+++ server/src/index.js
@@ -45,6 +45,19 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Serve frontend in production
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
 // Initialize database and start server
 async function startServer() {
   try {
